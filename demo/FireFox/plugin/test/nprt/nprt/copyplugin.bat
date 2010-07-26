e527398797ddbc5694a3e1b873762ffefba72d5c
stlt1sean <stlt1sean@aa5d9edc-7c6f-d5c9-3e23-ee20326b5c4f>
2010-07-26 11:40:11 +0000

    Scriptable Plugin Test: Compile "npruntime" code

---
+++ demo/FireFox/plugin/test/nprt/nprt/copyplugin.bat
@@ -0,0 +1,3 @@
+copy /y ..\debug\nprt.dll "C:\Program Files\Mozilla Firefox\plugins\nprt.dll"
+echo "To see the debug console, close all existed firefox.exe instances."
+"C:\Program Files\Mozilla Firefox\firefox.exe" ".\test.html" -console
