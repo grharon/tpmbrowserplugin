c018c6ad21a1497cb6bbece2052e918dcf1a683d
stlt1sean <stlt1sean@aa5d9edc-7c6f-d5c9-3e23-ee20326b5c4f>
2010-07-26 11:40:11 +0000

    Scriptable Plugin Test: Compile "npruntime" code

---
+++ trunk/demo/FireFox/plugin/test/nprt/nprt/ScriptablePluginObject.cpp
@@ -0,0 +1,120 @@
+#include "ScriptablePluginObject.h"
+#include "ConstructablePluginObject.h"
+#include "fix.h"
+
+bool
+ScriptablePluginObject::HasMethod(NPIdentifier name)
+{
+  return name == sFoo_id;
+}
+
+bool
+ScriptablePluginObject::HasProperty(NPIdentifier name)
+{
+  return (name == sBar_id ||
+          name == sPluginType_id);
+}
+
+bool
+ScriptablePluginObject::GetProperty(NPIdentifier name, NPVariant *result)
+{
+  VOID_TO_NPVARIANT(*result);
+
+  if (name == sBar_id) {
+    static int a = 17;
+
+    INT32_TO_NPVARIANT(a, *result);
+
+    a += 5;
+
+    return true;
+  }
+
+  if (name == sPluginType_id) {
+    NPObject *myobj =
+      NPN_CreateObject(mNpp, GET_NPOBJECT_CLASS(ConstructablePluginObject));
+    if (!myobj) {
+      return false;
+    }
+
+    OBJECT_TO_NPVARIANT(myobj, *result);
+
+    return true;
+  }
+
+  return true;
+}
+
+bool
+ScriptablePluginObject::Invoke(NPIdentifier name, const NPVariant *args,
+                               uint32_t argCount, NPVariant *result)
+{
+  if (name == sFoo_id) {
+    printf ("foo called!\n");
+	//MessageBox(NULL,"foo called!","ScriptablePlugin Sample",MB_OK);
+	
+    NPVariant docv;
+    NPN_GetProperty(mNpp, sWindowObj, sDocument_id, &docv);
+
+    NPObject *doc = NPVARIANT_TO_OBJECT(docv);
+
+    NPVariant strv;
+    STRINGZ_TO_NPVARIANT("div", strv);
+
+    NPVariant divv;
+    NPN_Invoke(mNpp, doc, sCreateElement_id, &strv, 1, &divv);
+
+    STRINGZ_TO_NPVARIANT("I'm created by a plugin!", strv);
+
+    NPVariant textv;
+    NPN_Invoke(mNpp, doc, sCreateTextNode_id, &strv, 1, &textv);
+
+    NPVariant v;
+    NPN_Invoke(mNpp, NPVARIANT_TO_OBJECT(divv), sAppendChild_id, &textv, 1,
+               &v);
+    NPN_ReleaseVariantValue(&v);
+
+    NPN_ReleaseVariantValue(&textv);
+
+    NPVariant bodyv;
+    NPN_GetProperty(mNpp, doc, sBody_id, &bodyv);
+
+    NPN_Invoke(mNpp, NPVARIANT_TO_OBJECT(bodyv), sAppendChild_id, &divv, 1,
+               &v);
+    NPN_ReleaseVariantValue(&v);
+
+    NPN_ReleaseVariantValue(&divv);
+    NPN_ReleaseVariantValue(&bodyv);
+
+    NPN_ReleaseVariantValue(&docv);
+	
+
+    STRINGZ_TO_NPVARIANT(m_strdup("foo return val"), *result);
+    return true;
+  }
+
+  return false;
+}
+
+bool
+ScriptablePluginObject::InvokeDefault(const NPVariant *args, uint32_t argCount,
+                                      NPVariant *result)
+{
+  printf ("ScriptablePluginObject default method called!\n");
+
+  STRINGZ_TO_NPVARIANT(m_strdup("default method return val"), *result);
+
+  return true;
+}
+
+bool
+ScriptablePluginObject::SetProperty(NPIdentifier name,
+                                        const NPVariant *value)
+{
+  if (name == sBar_id) {
+    printf ("bar set\n");
+
+    return true;
+  }
+  return false;
+}
---
+++ trunk/demo/FireFox/plugin/test/nprt/nprt/ScriptablePluginObject.h
@@ -0,0 +1,42 @@
+#ifndef SCRIPTABLEPLUGINOBJECT_H
+#define SCRIPTABLEPLUGINOBJECT_H
+#include "ScriptablePluginObjectBase.h"
+
+static NPIdentifier sFoo_id;
+static NPIdentifier sBar_id;
+static NPIdentifier sDocument_id;
+static NPIdentifier sBody_id;
+static NPIdentifier sCreateElement_id;
+static NPIdentifier sCreateTextNode_id;
+static NPIdentifier sAppendChild_id;
+static NPIdentifier sPluginType_id;
+static NPObject *sWindowObj;
+
+class ScriptablePluginObject : public ScriptablePluginObjectBase
+{
+public:
+  ScriptablePluginObject(NPP npp)
+    : ScriptablePluginObjectBase(npp)
+  {
+  }
+
+  virtual bool HasMethod(NPIdentifier name);
+  virtual bool HasProperty(NPIdentifier name);
+  virtual bool GetProperty(NPIdentifier name, NPVariant *result);
+  virtual bool Invoke(NPIdentifier name, const NPVariant *args,
+                      uint32_t argCount, NPVariant *result);
+  virtual bool InvokeDefault(const NPVariant *args, uint32_t argCount,
+                             NPVariant *result);
+  virtual bool SetProperty(NPIdentifier name, const NPVariant *value);
+};
+
+static NPObject *
+AllocateScriptablePluginObject(NPP npp, NPClass *aClass)
+{
+  return new ScriptablePluginObject(npp);
+}
+
+DECLARE_NPOBJECT_CLASS_WITH_BASE(ScriptablePluginObject,
+                                 AllocateScriptablePluginObject);
+
+#endif
