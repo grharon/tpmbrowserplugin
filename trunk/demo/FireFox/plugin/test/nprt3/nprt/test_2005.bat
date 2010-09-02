9e78d1918d6500ee87aa14cbd44d3a532d785d01
stlt1sean <stlt1sean@aa5d9edc-7c6f-d5c9-3e23-ee20326b5c4f>
2010-09-02 17:26:17 +0000

    cleanup for nprt3, and add its own /plugin directory

--- trunk/demo/FireFox/plugin/test/nprt3/nprt/test_2005.bat
+++ trunk/demo/FireFox/plugin/test/nprt3/nprt/test_2005.bat
@@ -1,3 +1,6 @@
-copy /y ..\Debug\nprt2_2005.dll "C:\Program Files\Mozilla Firefox\plugins\nprt.dll"
+pause
+copy /y ..\Debug\*.dll "C:\Program Files\Mozilla Firefox\plugins\"
+copy /y ..\Debug\*.exe "C:\Program Files\Mozilla Firefox\plugins\"
+
 echo "To see the debug console, close all existed firefox.exe instances."
 "C:\Program Files\Mozilla Firefox\firefox.exe" "https://10.239.47.82:8443/splat/identityservice/home?idp=idp1" -console
